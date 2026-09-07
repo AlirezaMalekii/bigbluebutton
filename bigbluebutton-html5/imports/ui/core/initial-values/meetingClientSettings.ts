9c0b48e9c39acc4dad1e603de9fed3f34dacebb1
safemeet-ci <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-07 12:44:10 +0000

    chore(version): bump SafeMeet to 0.8.40 [skip version] [skip ci]

--- bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
+++ bigbluebutton-html5/imports/ui/core/initial-values/meetingClientSettings.ts
@@ -18,7 +18,7 @@ export const meetingClientSettingsInitialValues: MeetingClientSettings = {
       bbbServerVersion: 'HTML5_FULL_BBB_VERSION',
       displayBbbServerVersion: false,
       copyright: '',
-      html5ClientBuild: '0.8.39',
+      html5ClientBuild: '0.8.40',
       helpLink: '',
       delayForUnmountOfSharedNote: 120000,
       enableApolloDevTools: false,
